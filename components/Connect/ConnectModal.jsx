b9141685eed8ef65dc98e3269c547c265fcc4620
diluk angelo <diluk@surge.global>
2023-02-14 23:54:41 +0530

    mobile update

--- components/Connect/ConnectModal.jsx
+++ components/Connect/ConnectModal.jsx
@@ -35,7 +35,7 @@ export default function ConnectModal() {
       return (
         <a
           className="btn btn-clear"
-          href="https://metamask.app.link/dapp/mintogac.com"
+          href="https://metamask.app.link/dapp/unicornsdapp.vercel.app/
         >
           <img src="/mbtn.png" alt="metamask button" width="40" /> Metamask
         </a>
